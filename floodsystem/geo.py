84c3211685faf36da8364e3fe133f4d617d02d95
chrislixj <chrislixj@gmail.com>
2022-01-22 17:31:29 +0000

    Added station_by_distance function

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,11 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from math import sqrt
+
+def station_by_distance(stations, p):
+	output_list = []
+	for station in stations:
+		distance = sqrt((station.coord[0]-p[0])**2+(station.coord[1]-p[1])**2)
+		output_list.append((station,distance))
+	return sorted(output_list, key=lambda x: x[1])
