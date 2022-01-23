ed34620b7a1fbc952e18704fdeab9f58d678d056
Chloe <chloe.yiu.yee.man@gmail.com>
2022-01-23 21:29:16 +0000

    Added test for rivers_by_station_number function

--- test_geo.py
+++ test_geo.py
@@ -31,3 +31,55 @@ def test_stations_by_river():
     for i in stations_by_river_dict.keys():
         for j in stations_by_river_dict[i]:
             assert j.river == i
+
+def test_rivers_by_station_number():
+    stations = build_station_list()
+    N = 9
+    river_dict = stations_by_river(stations)
+    
+    #check river_dict is a dictionary
+    assert type(river_dict) == type(dict())
+
+    river_list = []
+    
+    for river_name, station_obj_list in river_dict.items():
+        river_tuple = (river_name, len(station_obj_list))
+       
+        #check river_tuple is a tuple
+        assert type(river_tuple) == type(tuple())
+        #check river_tuple is of length 2
+        assert len(river_tuple) == 2
+
+        river_list.append(river_tuple)
+
+    greatest_list = sorted(river_list, key = lambda x: x[1], reverse=True)
+
+    #check greatest_list contains all needed items
+    assert len(greatest_list) == len(river_dict)
+    #check greatest_list is sorted
+    assert greatest_list[0][1] > greatest_list[-1][1]
+
+    greatest_N_list = greatest_list[:N]
+
+    #check greatest_N_list length = N
+    assert len(greatest_N_list) == N
+
+    for i in range(N):
+        greatest_list.pop(0)
+
+    #check greatest_list length = original length - N
+    assert len(greatest_list) == len(river_dict) - N
+    #check content in greatest_list & greatest_N_list is a tuple
+    assert type(greatest_list[0]) == type(tuple())
+    assert type(greatest_N_list[-1]) == type(tuple())
+    #check content in tuples are integers
+    assert type(greatest_list[0][1]) == type(int())
+    assert type(greatest_N_list[-1][1]) == type(int())
+
+    while True:
+        if greatest_list[0][1] == greatest_N_list[-1][1]:
+            greatest_N_list.append(greatest_list[0])
+            greatest_list.pop(0)
+        else:
+            break
+    return greatest_N_list
