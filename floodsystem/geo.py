b755e8bc0cf375326c4ac8a04b77ba4c3331038b
Chloe <chloe.yiu.yee.man@gmail.com>
2022-01-23 21:28:38 +0000

    Added rivers_by_station_number function

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -47,3 +47,31 @@ def stations_by_river(stations):
 		else:
 			output_dict[station.river]=[station]
 	return output_dict
+
+def rivers_by_station_number(stations, N):
+
+    """Takes in a list of stations object and an integer N
+	
+	Returns a list of tuples that shows the name of top N rivers 
+	with the greatest number of monitoring stations, and their respective numbers
+
+    The tuples are arranged from the ones with the most stations to the least stations
+
+    """
+
+    river_dict = stations_by_river(stations)
+    river_list = [] 
+    for river_name, station_obj_list in river_dict.items():
+        river_tuple = (river_name, len(station_obj_list))
+        river_list.append(river_tuple)
+    greatest_list = sorted(river_list, key = lambda x: x[1], reverse=True)
+    greatest_N_list = greatest_list[:N]
+    for i in range(N):
+        greatest_list.pop(0)
+    while True:
+        if greatest_list[0][1] == greatest_N_list[-1][1]:
+            greatest_N_list.append(greatest_list[0])
+            greatest_list.pop(0)
+        else:
+            break
+    return greatest_N_list
