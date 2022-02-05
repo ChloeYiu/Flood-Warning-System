52c327f8a6c6d01a7bff1230975a3ede69292c27
Chloe <chloe.yiu.yee.man@gmail.com>
2022-02-05 15:39:10 +0000

    Added functions for Task 2F

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates
 from datetime import datetime, timedelta
+from .analysis import *
 
 def plot_water_levels(station, dates, levels):
 	'''Plots the provided data using matplotlib, alongside lines indicating the typical range for the station'''
@@ -18,4 +20,25 @@ def plot_water_levels(station, dates, levels):
 	# Display plot
 	plt.tight_layout()  # This makes sure plot does not cut off date labels
 
+	plt.show()
+
+def plot_water_level_with_fit(station, dates, levels, p):
+	'''Plots the provided data using matplotlib, alongside lines indicating the best-fit polynomial details for the station'''
+	# Create list of time and list of level
+	x = matplotlib.dates.date2num(dates)
+	y = levels
+
+	# Retrieve best-fit polynomial details
+	poly, d0 = polyfit(dates, levels, p) 
+
+	# Plot
+	plt.plot(dates, y, '.')
+	plt.plot(dates, poly(x - d0))
+	plt.xlabel('date')
+	plt.ylabel('water level')
+	plt.xticks(rotation=45)
+	plt.title(station.name)
+
+	# Display plot
+	plt.tight_layout()  # This makes sure plot does not cut off date labels
 	plt.show()
